6f2cf587a66417f13c69c502034f0ecc59c33ba6
Daniel Gyoerffy <judalion777@gmail.com>
2022-06-14 22:05:17 +0200

    :white_check_mark: Update tests

--- core/src/test/java/org/binomial/distribution/core/PoweredMemberTest.java
+++ core/src/test/java/org/binomial/distribution/core/PoweredMemberTest.java
@@ -22,7 +22,7 @@ public class PoweredMemberTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {2, 3, 4, 5, 6, 7, 8, 9, 10, 1100})
+    @ValueSource(ints = {-2300, -5, -3, -1, 2, 3, 8, 1100})
     void itShouldRaiseIt(int exponent) {
         var underTest = new PoweredMember("a", exponent);
         assertEquals("a" + new ExponentNumber(exponent), underTest.toString());
